cae594ebc4059e4007f27588a0c6c6952a2810d0
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2023-07-18 19:18:53 +0300

    Fix sound level for announcer voicelines

--- gamemodes/nh2/gamemode/soundscripts/announcer_soundfiles.lua
+++ gamemodes/nh2/gamemode/soundscripts/announcer_soundfiles.lua
@@ -1,266 +1,266 @@
 sound.Add( {
 	name = "ann_lockdownremoved",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/Announcer/ann_lockdownremoved.wav"
 } )
 
 sound.Add( {
 	name = "ann_welcome",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/Announcer/ann_welcome.wav"
 } )
 
 sound.Add( {
 	name = "ann_0",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_0.wav"
 } )
 
 sound.Add( {
 	name = "ann_1",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_1.wav"
 } )
 
 sound.Add( {
 	name = "ann_10",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_10.wav"
 } )
 
 sound.Add( {
 	name = "ann_2",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_2.wav"
 } )
 
 sound.Add( {
 	name = "ann_3",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_3.wav"
 } )
 
 sound.Add( {
 	name = "ann_4",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_4.wav"
 } )
 
 sound.Add( {
 	name = "ann_5",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_5.wav"
 } )
 
 sound.Add( {
 	name = "ann_6",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_6.wav"
 } )
 
 sound.Add( {
 	name = "ann_7",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_7.wav"
 } )
 
 sound.Add( {
 	name = "ann_8",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_8.wav"
 } )
 
 sound.Add( {
 	name = "ann_9",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_9.wav"
 } )
 
 sound.Add( {
 	name = "ann_accesstoprivaterooms",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_accesstoprivaterooms.wav"
 } )
 
 sound.Add( {
 	name = "ann_arearestricted",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_arearestricted.wav"
 } )
 
 sound.Add( {
 	name = "ann_behindyou",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_behindyou.wav"
 } )
 
 sound.Add( {
 	name = "ann_contactanurse",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_contactanurse.wav"
 } )
 
 sound.Add( {
 	name = "ann_dingdong",
 	channel = CHAN_AUTO,
-	level = SNDLVL_NONE,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_dingdong.wav"
 } )
 
 sound.Add( {
 	name = "ann_dontgoout",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_dontgoout.wav"
 } )
 
 sound.Add( {
 	name = "ann_firedrill",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_firedrill.wav"
 } )
 
 sound.Add( {
 	name = "ann_firewarning",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_firewarning.wav"
 } )
 
 sound.Add( {
 	name = "ann_itsforyourowngood",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_itsforyourowngood.wav"
 } )
 
 sound.Add( {
 	name = "ann_leavinghospital",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_leavinghospital.wav"
 } )
 
 sound.Add( {
 	name = "ann_loadingbaygate",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_loadingbaygate.wav"
 } )
 
 sound.Add( {
 	name = "ann_loadingbaygate2",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_loadingbaygate2.wav"
 } )
 
 sound.Add( {
 	name = "ann_lockdown",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_lockdown.wav"
 } )
 
 sound.Add( {
 	name = "ann_lockdownremoved",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_lockdownremoved.wav"
 } )
 
 sound.Add( {
 	name = "ann_maintenancearea",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_maintenancearea.wav"
 } )
 
 sound.Add( {
 	name = "ann_notifyyou",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_notifyyou.wav"
 } )
 
 sound.Add( {
 	name = "ann_patientleaving",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_patientleaving.wav"
 } )
 
 sound.Add( {
 	name = "ann_radiologydepartment",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_radiologydepartment.wav"
 } )
 
 sound.Add( {
 	name = "ann_securitycheckpoint",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_securitycheckpoint.wav"
 } )
 
 sound.Add( {
 	name = "ann_securitygateopening1",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_securitygateopening1.wav"
 } )
 
 sound.Add( {
 	name = "ann_securitygateopening2",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_securitygateopening2.wav"
 } )
 
 sound.Add( {
 	name = "ann_surgerydepartment",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_surgerydepartment.wav"
 } )
 
 sound.Add( {
 	name = "ann_wecareaboutyou",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_wecareaboutyou.wav"
 } )
 
 sound.Add( {
 	name = "ann_yesyou",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_yesyou.wav"
 } )
 
 sound.Add( {
 	name = "ann_dontlookatme",
-	channel = CHAN_VOICE,
-	level = SNDLVL_NONE,
+	channel = CHAN_STATIC,
+	level = 0,
 	sound = "nh2/speech/announcer/ann_dontlookatme.wav"
 } )
 
